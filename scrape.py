fb778818031870a62544ac8309f2f8258a5e058a
Chun Kit Yip <47329780+chunkityip@users.noreply.github.com>
2019-08-21 16:56:40 -0400

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+source = requests.get('https://www.nytimes.com').text
+#To get source code from NewYorkTime(NYT) webiste by using the requests library
+#To get the source code from response object , add text
+#https://2.python-requests.org/en/master/user/quickstart/#response-content
+
+soup = BeautifulSoup(source, 'lxml')
+# lxml is a library that processing XML and HTML
+
+tag = soup.findAll('h2')
+# I found out that all news title in NYT website are using h2
+#<h2 class="css-1qwxefa esl82me0">
+#<span...........>Deficit Will Reach $1 Trillion Next Year, Budget Office Predicts</span>
+#</h2>
+#The reason of using h2 but not sapn is because span tag is for Big news. I want to print all the news
+
+file = open('news.csv', 'w')
+writer = csv.writer(file)
+writer.writerow(['Today news at New York Time:'])
+
+for article in tag:
+    news = article.text
+    print(news)
+
+    writer.writerow([news])
+file.close()
+
+#print all the news into csv file call news.csv
